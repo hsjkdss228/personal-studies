fb03780376aa7cc2087e886073bcfb4a5f78a146
Inwoo Hwang <hsjkdss228@naver.com>
2022-09-24 16:53:06 +0900

    14주차 4. 디자인 시스템-1

--- makaobank/frontend/src/App.jsx
+++ makaobank/frontend/src/App.jsx
@@ -1,5 +1,8 @@
 import { Routes, Route } from 'react-router-dom';
 
+import { Reset } from 'styled-reset';
+import styled from 'styled-components';
+
 import Header from './components/Header';
 
 import AccountPage from './pages/AccountPage';
@@ -7,16 +10,28 @@ import HomePage from './pages/HomePage';
 import TransactionsPage from './pages/Transasctions';
 import TransferPage from './pages/TransferPage';
 
+import GlobalStyle from './styles/GlobalStyle';
+
+const Main = styled.main`
+  padding: 1em;
+`;
+
 export default function App() {
+  // const Component = useRouteComponent();
+
   return (
     <div>
+      <Reset />
+      <GlobalStyle />
       <Header />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/account" element={<AccountPage />} />
-        <Route path="/transfer" element={<TransferPage />} />
-        <Route path="/transactions" element={<TransactionsPage />} />
-      </Routes>
+      <Main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/account" element={<AccountPage />} />
+          <Route path="/transfer" element={<TransferPage />} />
+          <Route path="/transactions" element={<TransactionsPage />} />
+        </Routes>
+      </Main>
     </div>
   );
 }
